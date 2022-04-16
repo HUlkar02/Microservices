44beac8415d5c99c5330c30159a5ba623964a195
Admin <usmonovahulkar1999@gmail.com>
2022-04-16 08:04:05 +0500

    commit 3

---
+++ ParentProject/Chat/src/test/java/pdp/uz/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package pdp.uz.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
